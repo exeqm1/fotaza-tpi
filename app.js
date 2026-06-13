c517b26219b1081c520f1e0fd7b3206426f79a7d
exeqm1 <boschieze6@gmail.com>
2026-06-12 23:50:25 -0300

    cambios port render

--- app.js
+++ app.js
@@ -88,17 +88,13 @@ app.post('/admin/strike/:id', requireAdmin, async (req, res) => {
 
 const PORT = process.env.PORT || 3000;
 
+app.listen(PORT, () => {
+    console.log(`Servidor corriendo en el puerto ${PORT}`);
+});
+
 db.sequelize.authenticate()
     .then(() => {
         console.log('Conexión a la base de datos establecida con éxito.');
         return db.sequelize.sync();
     })
     .catch(error => console.error('No se pudo conectar a la base de datos:', error));
-
-if (process.env.NODE_ENV !== 'production') {
-    app.listen(PORT, () => {
-        console.log(`Servidor corriendo en el puerto ${PORT}`);
-    });
-}
-
-module.exports = app;
